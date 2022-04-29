b78f60025c96c38cc41e73e1385ae67497679d33
Wojtek Bazant <wbazant@vet.upenn.edu>
2022-04-29 08:38:47 -0400

    Remove unused class

--- src/main/java/org/veupathdb/service/sequence/service/Sequences.java
+++ src/main/java/org/veupathdb/service/sequence/service/Sequences.java
@@ -3,8 +3,6 @@ package org.veupathdb.service.sequence.service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import org.veupathdb.service.sequence.model.config.ExtOptions;
-
 import org.veupathdb.service.sequence.model.Config;
 import org.veupathdb.service.sequence.model.ReferenceSequenceSpec;
 
