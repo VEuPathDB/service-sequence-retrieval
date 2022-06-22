a1a46fba45bfa81ece450dc4d7290ed4a97c764b
Elizabeth <foxcapade@gmail.com>
2022-06-22 13:57:23 -0400

    update to use newest container core

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,7 +84,7 @@ repositories {
 dependencies {
 
   // Core lib
-  implementation("org.veupathdb.lib:jaxrs-container-core:6.5.1")
+  implementation("org.veupathdb.lib:jaxrs-container-core:6.7.0")
 
   // Jersey
   implementation("org.glassfish.jersey.core:jersey-server:3.0.4")
