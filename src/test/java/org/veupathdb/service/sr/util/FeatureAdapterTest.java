54b313418122b4ba045d00ddac042d9fc981cedf
Wojtek Bazant <wbazant@vet.upenn.edu>
2022-11-03 14:39:31 +0000

    Strand can be null

---
+++ src/test/java/org/veupathdb/service/sr/util/FeatureAdapterTest.java
@@ -0,0 +1,46 @@
+package org.veupathdb.service.sr.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.veupathdb.service.sr.service.SequenceRetrievalService;
+import org.veupathdb.service.sr.generated.model.*;
+
+import org.veupathdb.service.sr.util.Sequences;
+import org.veupathdb.service.sr.config.TestReferenceSequenceConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.core.Response;
+
+import org.veupathdb.service.sr.TestFiles;
+import org.veupathdb.service.sr.generated.model.DeflineFormat;
+import org.veupathdb.service.sr.generated.model.StartOffset;
+import org.veupathdb.service.sr.generated.model.SequencesSequenceTypeFileFormatPostMultipartFormDataImpl;
+import org.veupathdb.service.sr.generated.model.UploadMethod;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+
+
+public class FeatureAdapterTest {
+
+  @BeforeEach
+  public void setUp(){
+    Sequences.initialize(new TestReferenceSequenceConfig());
+  }
+
+  @Test
+  public void testReadBed() throws Exception {
+    var features = FeatureAdapter.readBed(new FileInputStream(TestFiles.proteinBed()), StartOffset.ZERO);
+    assertEquals(features.size(), 1);
+    assertEquals(features.get(0).getContig(), "EHI7A_117830-t26_1-p1");
+    assertEquals(features.get(0).getStart(), 1);
+    assertEquals(features.get(0).getEnd(), 7);
+    assertEquals(features.get(0).getName(), "");
+
+  }
+}
