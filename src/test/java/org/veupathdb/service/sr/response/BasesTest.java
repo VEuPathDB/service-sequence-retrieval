a26d4dc47603be14e4835818c8fb7be1dac6dd71
Ryan Doherty <tech@conical.org>
2023-10-18 12:14:26 -0400

    Fix imports in unit test

--- src/test/java/org/veupathdb/service/sr/response/BasesTest.java
+++ src/test/java/org/veupathdb/service/sr/response/BasesTest.java
@@ -1,36 +1,15 @@
 package org.veupathdb.service.sr.response;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import java.io.IOException;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.veupathdb.service.sr.service.SequenceRetrievalService;
-import org.veupathdb.service.sr.generated.model.*;
-
-import org.veupathdb.service.sr.TestReferences;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.ws.rs.core.Response;
-
-import org.veupathdb.service.sr.TestQueries;
-import org.veupathdb.service.sr.generated.model.DeflineFormat;
-import org.veupathdb.service.sr.generated.model.StartOffset;
-import org.veupathdb.service.sr.generated.model.SequencesSequenceTypeFileFormatPostMultipartFormDataImpl;
-import org.veupathdb.service.sr.generated.model.UploadMethod;
-import org.veupathdb.service.sr.generated.model.SequenceType;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-
-import htsjdk.tribble.bed.BEDFeature;
-import htsjdk.tribble.bed.SimpleBEDFeature;
-import htsjdk.tribble.bed.FullBEDFeature;
-import htsjdk.tribble.annotation.Strand;
 import htsjdk.samtools.reference.ReferenceSequence;
-import htsjdk.samtools.reference.IndexedFastaSequenceFile;
+import htsjdk.tribble.annotation.Strand;
+import htsjdk.tribble.bed.BEDFeature;
+import htsjdk.tribble.bed.FullBEDFeature;
+import htsjdk.tribble.bed.SimpleBEDFeature;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasesTest {
 
